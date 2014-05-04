b6420f1b2c0417ed22f12ac97ee00d3cb0ba8830
NK <beohemian@gmail.com>
2014-05-05 07:54:58 +0900

    1. updata google place 	- 	API Request 를 통해서 		JSON Data 를 받아서 Dictionary 형태로 저장하는데 까지 성공.

--- tfd_bot_python/src/parser/googleplace.py
+++ tfd_bot_python/src/parser/googleplace.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 '''
 Created on 2014. 5. 3.
 
@@ -11,18 +13,33 @@ https://maps.googleapis.com/maps/api/place/photo?maxwidth=""&photoreference=""&s
 
 '''
 
+import urllib2
+import json
+
 class GooglePlace(object):
     def __init__(self):
         self._API_KEY = "AIzaSyCJQXicui49gzhAwbGWGmm3OJEpKPjUeJE"
-        self._BASE_URL = "https://maps.googleapis.com/maps/api/place/"
-        self._parameters = {}
+        self._BASE_URL = "https://maps.googleapis.com/maps/api/place"
         self._returnType = "json"
         self._language = "ko"
-        
+        self._sensor = "true"
     def getResult(self, keyword):
-        query = keyword
+        query = keyword.replace(" ", "+")
+        print query
         function = "textsearch"
-        url = "/".join(self._BASE_URL, function)
+        url = "/".join([self._BASE_URL, function])
+        url = "/".join([url, self._returnType])
+        
+        param = {"query":query,
+                 "sensor":self._sensor,
+                 "language":self._language,
+                 "key":self._API_KEY
+                 }
+        print param
+        completParam = self._generateParam(param)
+        url = "?".join([url, completParam])
+        print url
+        self._connect(url)
         
     def getDetailResult(self):
         function = "details"
@@ -31,5 +48,30 @@ class GooglePlace(object):
         function = "photo"
         
     def _connect(self, url):
-        pass
+        try:
+            #구글 플레이스에서 API 리턴값을 받는다. 
+            data = urllib2.urlopen(url).read()            
+            #JSON 값을 Dictionary로 Converse.
+            data = json.loads(data)            
+            # DB에 넣기 전 데이터 가공단계.
+            self._generateData(data)
+            
+        except urllib2.HTTPError, e:
+            print "HTTP error: %d" % e.code
+        except urllib2.URLError, e:
+            print "Network error: %s" % e.reason.args[1]
+        
+    def _generateParam(self, param):
+        list = []
+        for k in param.keys():
+            list.append("=".join([k,param[k]]))
+        return "&".join(list)
+    def _generateData(self, data):
+        resultList = data["results"]
+        newData = {}
+        
     
+if __name__ == "__main__":
+    gp = GooglePlace()
+    gp.getResult("홍대 식당")
+        
